555fd841d5326e12cba37c71e417fe8886adc4a2
vincenteof <14847208+vincenteof@users.noreply.github.com>
2023-02-11 00:38:47 +0800

    feat: adjust layout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -5,25 +5,6 @@ export default function Index() {
   return (
     <section>
       <article>
-        <p className="my-2">
-          Hi, i am vincenteof. I am a <b>frontend developer</b> living in
-          Shanghai.
-        </p>
-        <p className="my-2">
-          I mainly focus on frontend development, and i also have a strong
-          belief in blockchain technology like <b>ethereum</b>.{" "}
-          <NavLink to="about">Read more about me</NavLink>
-        </p>
-
-        <p className="my-2">
-          After regularly working in tech companies for about five years, i am
-          currently looking for some new way of life, such as being a digital
-          nomad, participating in OSS or starting a startup.
-        </p>
-        <p className="my-2">
-          If you think there are some opportunity we could cooperate with each
-          other, please do not hesitate to contact me.
-        </p>
         <div className="flex items-start md:items-center my-8 flex-col md:flex-row">
           <img
             className="rounded-full w-[100px] h-[100px]"
@@ -31,7 +12,26 @@ export default function Index() {
             src="https://avatars.githubusercontent.com/u/14847208?v=4"
           />
           <div className="mt-8 md:mt-0 ml-0 md:ml-6 space-y-2 text-neutral-500 dark:text-neutral-400">
-            <p className="flex items-center gap-2">
+            <p> Hi, i am vincenteof. </p>
+            <p className="my-2">
+              I am a <b>frontend developer</b> living in Shanghai.
+            </p>
+          </div>
+        </div>
+        <div className="pl-8">
+          <p className="my-6 max-w-2xl">
+            I mainly focus on frontend development, and i also have a strong
+            belief in blockchain technology like <b>ethereum</b>.{" "}
+            <NavLink to="about">Read more about me</NavLink>
+          </p>
+          <p className="pb-10 mt-6 max-w-2xl">
+            After regularly working in tech companies for about five years, i am
+            currently looking for some new way of life, such as being a digital
+            nomad, participating in OSS or starting a startup.
+          </p>
+          <hr className="w-20" />
+          <div className="pt-4 text-neutral-500">
+            <p className="flex items-center gap-2 ">
               Find me on
               <a
                 href="https://github.com/vincenteof"
@@ -55,11 +55,15 @@ export default function Index() {
             </p>
             <p>
               Mail me at{" "}
-              <a href="mailto:vincenteofchen@gmail.com">
+              <a href="mailto:vincenteofchen@gmail.com" className="underline">
                 vincenteofchen@gmail.com
               </a>
             </p>
           </div>
+          <p className="mt-3 max-w-2xl text-neutral-500">
+            If you think there are some opportunity we could cooperate with each
+            other, please do not hesitate to contact me.
+          </p>
         </div>
       </article>
     </section>
